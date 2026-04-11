a86e3ecab80d0c79c6bbb464f85a191c36d84ee3
shagzodbek334-coder <shagzodbek334@gmail.com>
2026-04-11 13:56:24 +0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   const [data, setData] = useState([])
+  console.log(data);
+
   const getData = () => {
     const requestOptions = {
       method: "GET",
@@ -28,11 +30,18 @@ function App() {
               Vazifalar ro'yxati
             </h2>
           </div>
+          <div className="dark">
+            <button>
+              Yangi vazifa qo'shish
+            </button>
+          </div>
 
-         
+
 
         </nav>
       </div>
+
+
 
       <div className="container">
         <div className="flex">
@@ -42,14 +51,13 @@ function App() {
                 <div className="card" >
                   <div className="card-header">
                     <h3>{item?.title}</h3>
-                    <span className="status">{item?.status}</span>
                   </div>
 
                   <p className="desc">{item?.description}</p>
 
                   <div className="date">
-                    <span>{item?.start_date}</span>
-                    <span>{item?.end_date}</span>
+                    <span>{item?.deadline}</span>
+                    
                   </div>
 
                   <div className="actions">
